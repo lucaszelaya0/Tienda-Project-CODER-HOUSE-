2c8e8ade67824ab6f7280001cd0418b028c3ce3a
Lucas Ivan Zelaya <lucaszelaya108@gmail.com>
2021-02-14 22:42:29 -0300

    solucionado

--- js/productos.js
+++ js/productos.js
@@ -1,62 +1,62 @@
 const products = [
     {
-        "img":"img/remera.png",
+        "img":"remera.png",
         "productId":"1",
         inCart: 0,
         "nombre": "Remera Black",
         "precio" : 1000
     },
     {
-        "img":"img/remeraLeaf.png",
+        "img":"remeraLeaf.png",
         "productId":"2",
         inCart: 0,
         "nombre": "Remera Leaf",
         "precio" : 1200
     },
     {
-        "img":"img/remeraNavy.png",
+        "img":"remeraNavy.png",
         "productId":"3",
         inCart: 0,
         "nombre": "Remera Navy",
         "precio" : 1300
     },
     {
-        "img":"img/buzo.png",
+        "img":"buzo.png",
         "productId":"4",
         inCart: 0,
         "nombre": "Buzo Black",
         "precio" : 3000
     },
     {
-        "img":"img/buzoLeaf.png",
+        "img":"buzoLeaf.png",
         "productId":"5",
         inCart: 0,
         "nombre": "Buzo Leaf",
         "precio" : 3100
     },
     {
-        "img":"img/buzoNavy.png",
+        "img":"buzoNavy.png",
         "productId":"6",
         inCart: 0,
         "nombre": "Buzo Navy",
         "precio" : 3200
     },
     {
-        "img":"img/campera.png",
+        "img":"campera.png",
         "productId":"7",
         inCart: 0,
         "nombre": "Campera Black",
         "precio" : 2000
     },
     {
-        "img":"img/camperaLeaf.png",
+        "img":"camperaLeaf.png",
         "productId":"8",
         inCart: 0,
         "nombre": "Campera Leaf",
         "precio" : 2100
     },
     {
-        "img":"img/camperaNavy.png",
+        "img":"camperaNavy.png",
         "productId":"9",
         inCart: 0,
         "nombre": "Campera Navy",
